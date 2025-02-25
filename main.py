c4a7a8ff5641dc2c5d616c1d3232f6b56c40a078
stundrg <khr0106@naver.com>
2025-02-25 12:32:38 +0900

    머저리 같은 코드와 비교 하기 위한 커밋

--- main.py
+++ main.py
@@ -42,44 +42,52 @@ def two_dimensional_array():
 
 @app.get("/add-large-arrays")
 def add_large_arrays():
-    N = 10**6  
-
-    # 랜덤한 1차원 리스트 2개 생성
-    a = [random.random() for _ in range(N)]
-    b = [random.random() for _ in range(N)]
-
-    start_time = time.time()
-    
-    # for 루프를 이용한 요소별 덧셈
-    result = []
-    for i in range(N):
-        result.append(a[i] + b[i])
-
-    end_time = time.time()
-    
-    return {"execution_time": end_time - start_time}
-
-@app.get("/add-large-arrays_choice")
-def add_large_arrays_choice():
-    N = 10**6  
-    start_creation_time = time.time()
-    # 랜덤한 1차원 리스트 2개 생성
-    a = [random.choice(range(0, 100)) for _ in range(N)]
-    b = [random.choice(range(0, 100)) for _ in range(N)]
-
-    add_start_time = time.time()
-    
-    # for 루프를 이용한 요소별 덧셈
-    result = []
-    for i in range(N):
-        result.append(a[i] + b[i])
-
-    add_end_time = time.time()
-    end_creation_time = time.time()
+    N = 10**6  # 100만 개 요소
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_randint)
     return {
-        "execution_time": add_end_time - add_start_time,
-        "array_creation_time": end_creation_time - start_creation_time}
+        "array_creation_time": array_creation_time,
+        "addition_time": addition_time
+        }
+    
+@app.get("/add-large-arrays-choices")
+def add_large_arrays_choices():
+    N = 10**6  # 100만 개 요소
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_choices)
+    return {
+        "array_creation_time": array_creation_time,
+        "addition_time": addition_time
+        }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def gen_r_array_randint(N):
+    a = [random.randint(0, 100) for _ in range(N)]
+    b = [random.randint(0, 100) for _ in range(N)]
+    return a,b
+
+
+
+def add_arrays(N, fun):
+    # 랜덤한 1차원 배열 2개 생성
+    start_creation_time = time.time()
+    a, b = fun(N)
+    end_creation_time = time.time()
+    
+    # 실행 시간 측정 시작
+    add_start_time = time.time()
+    # 요소별 덧셈
+    result = []
+    for x, y in zip(a, b):
+        result.append(x + y)
+     
+    # 실행 시간 측정 종료
+    add_end_time = time.time()
+    
+    # 수행 시간 리턴
+    # 리턴값: 배열 생성 시간(array_creation_time)과 덧셈 수행(addition_time) 시간을 각각 리턴
+    array_creation_time = end_creation_time - start_creation_time
+    addition_time = add_end_time - add_start_time
+    return array_creation_time, addition_time
+
+def gen_r_array_choices(N):
+    a = random.choices(range(101), k=N)
+    b = random.choices(range(101), k=N)
+    return a,b
