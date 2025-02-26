4a2f7c3c3f86a894105493c94e0c8f4da3c1759f
stundrg <khr0106@naver.com>
2025-02-26 09:11:42 +0900

    작업 내용 저장

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def two_dimensional_array():
 
 @app.get("/add-large-arrays")
 def add_large_arrays():
-    N = 10**6  # 100만 개 요소
+    N = 10**5  # 100만 개 요소
     array_creation_time, addition_time = add_arrays(N, gen_r_array_randint)
     return {
         "array_creation_time": array_creation_time,
@@ -51,7 +51,7 @@ def add_large_arrays():
     
 @app.get("/add-large-arrays-choices")
 def add_large_arrays_choices():
-    N = 10**6  # 100만 개 요소
+    N = 10**5  # 100만 개 요소
     array_creation_time, addition_time = add_arrays(N, gen_r_array_choices)
     return {
         "array_creation_time": array_creation_time,
